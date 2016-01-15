a4fbb2b551d610bc3829436e2541aed323f6078d
LandRover <github.com@landroverz.com>
2016-01-15 09:49:53 +0000

    google search added

--- src/config.js
+++ src/config.js
@@ -6,6 +6,11 @@ var config = {
         phoneNumber: process.env.PHONE || null,
         password: process.env.PASS || null
     },
+    google: {
+        CSE: process.env.CSE || null,
+        apiKey: process.env.API_KEY || null
+    },
+
     tmp: {
         dir: __dirname + '/../_TMP'
     }
